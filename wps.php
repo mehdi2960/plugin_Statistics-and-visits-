35e98958c3bc670a4cc4a7c12130276b8999fe75
mehdi <mehdiprogrammer30@gmail.com>
2022-08-04 14:45:45 +0430

    define constants

--- wps.php
+++ wps.php
@@ -1,12 +1,21 @@
 <?php
-/**
- * Plugin Name: WP-Parsidate
- * Version: 1.0.0
- * Plugin URI: https://wp-parsi.com/support/
- * Description: Persian package for WordPress, Adds full RTL and Shamsi (Jalali) support for: posts, comments, pages, archives, search, categories, permalinks and all admin sections and TinyMce editor, lists, quick editor. This package has Jalali archive widget.
- * Author: WP-Parsi Team
- * Author URI: https://wp-parsi.com/
- * Text Domain: wp-parsidate
- * Domain Path: parsi-languages
- * License: GPL3
- **/
+/*
+ * Plugin Name: آمار بازدید کاربران
+ * Plugin URI: https://wp.div
+ * Description: پلاگین قدرتمند برای دریافت بازدید کاربران از سایت وردپرسی
+ * Author: mehdi mousavi
+ *  Version: 1.0.0
+ */
+
+// پلاگین بصورت مستقیم فایلش اجرا نشود
+defined('ABSPATH') || exit();
+
+//define constants
+define('WPS_DIR',trailingslashit(plugin_dir_path(__FILE__)));
+define('WPS_URL',trailingslashit(plugin_dir_url(__FILE__)));
+define('WPS_INC',trailingslashit(WPS_DIR.'inc'));
+define('WPS_CSS',trailingslashit(WPS_URL.'assets'.'/'.'css'));
+define('WPS_JS',trailingslashit(WPS_URL.'assets'.'/'.'js'));
+define('WPS_IMAGES',trailingslashit(WPS_URL.'assets'.'/'.'images'));
+define('WPS_FONTS',trailingslashit(WPS_URL.'assets'.'/'.'fonts'));
+
