9a5cb3da1fc86aace6f348977431f66765f9b0d9
mehdi <mehdiprogrammer30@gmail.com>
2022-08-04 14:20:56 +0430

    visits plugin

---
+++ wps.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP-Parsidate
+ * Version: 1.0.0
+ * Plugin URI: https://wp-parsi.com/support/
+ * Description: Persian package for WordPress, Adds full RTL and Shamsi (Jalali) support for: posts, comments, pages, archives, search, categories, permalinks and all admin sections and TinyMce editor, lists, quick editor. This package has Jalali archive widget.
+ * Author: WP-Parsi Team
+ * Author URI: https://wp-parsi.com/
+ * Text Domain: wp-parsidate
+ * Domain Path: parsi-languages
+ * License: GPL3
+ **/
